cc8b91f96d4674e5d17bb7b2490175da1e988243
Jorge <caifaguar.jm@gmail.com>
2017-04-27 22:09:21 -0500

    Se agregaron: una modulo importado (suma.h).

--- proy_1.c
+++ proy_1.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include </home/jorge/Escritorio/proyecto_git/len_c/suma.h>
 
 int main(){
-    
+    int a,b;
+
     printf("Hola mundo!!\n");
+    printf("Dame dos números enteros para sumarlos\n");
+    scanf("%d %d",&a,&b);
+    printf("\n%d + %d = %d\n", a,b,suma(a,b));
 
 	return 0;
 }
